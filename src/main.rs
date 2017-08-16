59ccf3e267fd9febcb5a1d84d214c7e63c290154
Justin Hurst <jryanhurst@gmail.com>
2017-08-16 13:56:10 -0600

    initialize new Application with OpenGL

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+extern crate piston;
+extern crate glutin_window;
+extern crate graphics;
+extern crate opengl_graphics;
+
+use piston::window::{WindowSettings};
+use piston::event_loop::{Events, EventLoop, EventSettings};
+use piston::input::{RenderEvent};
+use glutin_window::{GlutinWindow};
+use opengl_graphics::{OpenGL, GlGraphics};
+
 fn main() {
-    println!("Hello, world!");
+    let glVersion = OpenGL::V3_2;
+    let settings = WindowSettings::new("Rustlike", [512; 2])
+        .opengl(glVersion)
+        .exit_on_esc(true);
+
+    let mut window: GlutinWindow = settings.build()
+        .expect("Could not create window");
+
+    let mut events = Events::new(EventSettings::new().lazy(true));
+    let mut glGfx = GlGraphics::new(glVersion);
+
+    while let Some(evt) = events.next(&mut window) {
+        if let Some(args) = evt.render_args() {
+           glGfx.draw(args.viewport(), |_, gfx| {
+               use graphics::{clear};
+
+               clear([0.2; 4], gfx);
+           });
+        }
+    }
 }
