2cfd5ab656eacbca65fc02f324cf4ff8806b1fbf
Justin Hurst <jryanhurst@gmail.com>
2017-08-16 20:06:08 -0600

    Draw grid that represents world camera

--- src/game_view.rs
+++ src/game_view.rs
@@ -4,12 +4,23 @@ use graphics::{Context, Graphics};
 use GameController;
 
 /// Visual Configuration Information
-pub struct GameViewSettings {}
+pub struct GameViewSettings {
+    /// Position from Upper left corner
+    pub position: [f64; 2],
+    /// World Size in number of cells
+    pub size: u32,
+    /// Size of world cells
+    pub cell_size: u32,
+}
 
 impl GameViewSettings {
     /// Create new instance of GameViewSettings
     pub fn new() -> GameViewSettings {
-        return GameViewSettings {};
+        return GameViewSettings {
+            position: [16 as f64; 2],
+            size: 10,
+            cell_size: 48,
+        };
     }
 }
 
@@ -29,8 +40,24 @@ impl GameView {
 
     /// Draw Game
     pub fn draw<G: Graphics>(&self, _: &GameController, ctx: &Context, gfx: &mut G) {
-        use graphics::{Rectangle};
+        use graphics::{Line};
 
-        Rectangle::new([1.0; 4]).draw([24.0, 24.0, 100.0, 100.0 ], &ctx.draw_state, ctx.transform, gfx);
+        let ref settings = self.settings;
+
+        let cell_edge = Line::new([0.8; 4], 1.0);
+        for i in 0..settings.size+1 {
+            let x = settings.position[0] + i as f64  * settings.cell_size as f64;
+            let y = settings.position[1] + i as f64  * settings.cell_size as f64;
+            let x2 = settings.position[0] +
+                (settings.size as f64 * settings.cell_size as f64);
+            let y2 = settings.position[1] +
+                (settings.size as f64 * settings.cell_size as f64);
+
+            let vline = [x, settings.position[1], x, y2];
+            cell_edge.draw(vline, &ctx.draw_state, ctx.transform, gfx);
+
+            let hline = [settings.position[0], y, x2, y];
+            cell_edge.draw(hline, &ctx.draw_state, ctx.transform, gfx);
+        }
     }
 }
