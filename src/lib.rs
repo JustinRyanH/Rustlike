3652723ae3deb6e17431cfa5ed10774d765dd95b
Justin Hurst <jryanhurst@gmail.com>
2017-11-04 11:29:43 -0600

    Removed Context.rs until need for abstraction

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,6 @@ use sdl2::keyboard::Keycode;
 pub mod gl;
 pub mod error;
 pub mod builder;
-pub mod context;
 
 static VS_SRC: &'static str =
     "#version 150\n\
@@ -17,14 +16,53 @@ static VS_SRC: &'static str =
      gl_Position = vec4(position, 1.0);\n\
      }";
 
+struct Context {
+    
+}
+
+impl Context {
+    /// Iterates over the active SDL drivers and finds one used
+    /// for OpenGL communication
+    pub fn find_sdl_gl_driver() -> Option<u32> {
+        for (index, item) in sdl2::render::drivers().enumerate() {
+            if item.name == "opengl" {
+                return Some(index as u32);
+            }
+        }
+        None
+    }
+}
+
+
 pub fn run() -> error::AppResult<()> {
-    let mut context = context::Context::new(Default::default())?;
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+    let window = video_subsystem.window("Window", 800, 600)
+        .opengl()
+        .build()
+        .unwrap();
+    let mut canvas = window.into_canvas()
+        .index(Context::find_sdl_gl_driver().unwrap())
+        .build()
+        .unwrap();
 
-    let program = gl::shader::ShaderProgram::new(gl::shader::ShaderType::Vertex, vertexShaderSource)?;
+    gl::raw::load_with(|name| video_subsystem.gl_get_proc_address(name) as *const _);
+    canvas.window().gl_set_context_to_current();
 
+
+    let mut event_pump = sdl_context.event_pump().unwrap();
+
+
+    // let mut context = context::Context::new(Default::default())?;
     'running: loop {
-        context.blip();
-        for event in context.poll_iter() {
+    //     context.blip();
+        unsafe {
+            gl::raw::ClearColor(0.6, 0.0, 0.8, 1.0);
+            gl::raw::Clear(gl::raw::COLOR_BUFFER_BIT);
+        }
+
+        canvas.present();
+        for event in event_pump.poll_iter() {
                 match event {
                     Event::Quit {..} | Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
                         break 'running
