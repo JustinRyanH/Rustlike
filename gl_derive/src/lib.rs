f9259b0ca4115923007b5c8b53096556b9a186f5
Justin Hurst <jryanhurst@gmail.com>
2017-11-19 10:30:40 -0700

    Move gl into it's own directory

---
+++ gl_derive/src/lib.rs
@@ -0,0 +1,32 @@
+extern crate proc_macro;
+extern crate syn;
+#[macro_use] extern crate quote;
+
+
+use proc_macro::TokenStream;
+
+#[proc_macr_derive(rustlike::gl::attributes::DescribeAttributes)]
+pub fn describe_attributes(input: TokenSteam) -> TokenStream {
+    let s = input.to_string();
+    let ast = syn::parse_macro_input(&s).unwrap();
+}
+
+pub fn impl_describe_attributes(ast: &syn::MacroInput) -> quote::Tokens {
+    let name = &ast.ident;
+    quote! {
+        impl #name {
+            unsafe fn attributes() -> Vec<Attribute> {
+                Vec::new()
+            }
+        }
+    }
+}
+
+
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+    }
+}
